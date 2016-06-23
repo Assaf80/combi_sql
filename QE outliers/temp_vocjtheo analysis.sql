77793755cea83f1d8d6875f0515f345409b26f9a
Assaf80 <assaf.anderson@gmail.com>
2016-06-23 11:20:18 +0300

    joining mirror_vsamples
    in order to validate that the analyzed absorber is the last assumed
    active layer

--- QE outliers/temp_vocjtheo analysis.sql
+++ QE outliers/temp_vocjtheo analysis.sql
@@ -1,3 +1,6 @@
-select absorber,contact,count(*) from temp_voconjtheo
+select absorber,contact,materials,count(*) from temp_voconjtheo
+join mirror_vsamples on mirror_vsamples.sample_id= temp_voconjtheo.sample_id
 where (max_j_CW_LED<3 and IQE_percent >5)
-group by absorber,contact;
+group by absorber,contact,materials;
+
+select  * from temp_voconjtheo
